e1927450472b7278df6dffe50748717dc8eaa571
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-07-06 00:06:52 +0530

    Added EslintRc Simple Import Sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 
-import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
 
 function App() {
   return (
     <div className="text-3xl font-bold underline">
-      <Signup/>
+      <Signup />
     </div>
   );
 }
